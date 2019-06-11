4ea59a52b91d21f6c51df1c182d33e55d1d83b27
Rebecca Lisl Gold <rebecca.gold@mail.mcgill.ca>
2019-06-11 22:43:37 +0000

    completed exercise5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,8 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Total Revenue: #{Store.sum(:annual_revenue)}"
+puts "Average Revenue #{Store.average(:annual_revenue)}"
+
+puts "No. of stores generating over $1M in annual sales: #{Store.where("annual_revenue >= ?", 1000000).count}"
